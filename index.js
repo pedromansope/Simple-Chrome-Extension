ce834335bc9a370d16e358a285c43e6aa80fdae3
Pedro Manso <pedrolucasmppe@gmail.com>
2022-11-29 15:23:35 -0300

    implementando localstorage

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 let myLeads = []
+
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
+    console.log( localStorage.getItem(myLeads) )
 })
 
 function renderLeads() {
