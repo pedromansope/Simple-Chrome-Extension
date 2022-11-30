1a3113b36dd22ef39743665582ae70d0eba4afb3
Pedro Manso <pedrolucasmppe@gmail.com>
2022-11-30 14:48:28 -0300

    refactor: improving parameters to functions

--- index.js
+++ index.js
@@ -4,29 +4,14 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    renderLeads(myLeads)
 }
 
-deleteBtn.addEventListener('dblclick', function(){
-    localStorage.clear()
-    myLeads = []
-    renderLeads()
-  });
-
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
-    console.log( localStorage.getItem(myLeads) )
-})
-
-function renderLeads() {
+function renderLeads(leads) {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
         listItems += `
@@ -37,5 +22,18 @@ function renderLeads() {
             </li>
         `
     }
-    ulEl.innerHTML = listItems  
+    ulEl.innerHTML = listItems
 }
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    renderLeads()
+})
