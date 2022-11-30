a9ef6f041ffc30332ffe6d46310f93046c233808
Pedro Manso <pedrolucasmppe@gmail.com>
2022-11-30 15:13:13 -0300

    feat: Save tab button

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ if (leadsFromLocalStorage) {
 }
 
 
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
+
 function render(leads) {
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
